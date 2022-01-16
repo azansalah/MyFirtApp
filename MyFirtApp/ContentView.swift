1051d4154bb596eeaf900d6eff7dabafb4609c48
azansalah <49494969+azansalah@users.noreply.github.com>
2022-01-16 22:03:50 +0700

    Initial Commit

---
+++ MyFirtApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFirtApp
+//
+//  Created by Azan Salah on 16/1/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFirtApp/MyFirtAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirtAppApp.swift
+//  MyFirtApp
+//
+//  Created by Azan Salah on 16/1/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct MyFirtAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
